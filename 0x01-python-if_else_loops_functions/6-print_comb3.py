5bbbfb6a405740e4ad5a0622d15f437893d91141
Grace Effiong <graceffiong@gmail.com>
2022-05-31 19:29:23 +0000

    task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
-for i in range(1, 90):
-    print('{:02d}'.format(i), end='\n' if i == 89 else ', ')
+number = 0
+while number <= 89:
+    if number % 10 == 0:
+        number += 1 + number // 10
+    print("{:02d}".format(number), end='\n' if number == 89 else ", ")
+    number += 1
+
+'''
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2))
+        else:
+            print("{}{}".format(digit1, digit2), end=", ")
+'''
