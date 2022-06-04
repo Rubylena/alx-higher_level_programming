17033ae000d1ecde14749379c269dc7ce2b4a6a7
Grace Effiong <graceffiong@gmail.com>
2022-06-04 22:30:31 +0000

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -8,6 +8,4 @@ def element_at(my_list, idx):
     if idx > len(my_list):
         return None
 
-    for i in range(len(my_list)):
-        if idx == i:
-            return ('{}'.format(my_list[i]))
+    return (my_list[idx])
