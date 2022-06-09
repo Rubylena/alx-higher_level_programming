f9cf0ee4697737494314ab9f4899c605f44bcbf2
Grace Effiong <graceffiong@gmail.com>
2022-06-09 13:00:15 +0000

    task 2 to 5

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def uniq_add(my_list=[]):
+    result = 0
+    for x in set(my_list):
+        result += x
+    return result
+'''
+def uniq_add(my_list=[]):
+    return sum(set(my_list))
+'''
---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,6 @@
+
+#!/usr/bin/python3
+
+
+def only_diff_elements(set_1, set_2):
+    return (set_1 ^ set_2)
