69a50249719061977c901bfbac1c626773527e33
Grace Effiong <graceffiong@gmail.com>
2022-06-27 19:43:58 +0000

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,7 +3,15 @@
 
 
 class Square:
-    ''' intializing a class instance '''
+    ''' Represent a square.'''
+
 
     def __init__(self, size):
+        ''' intializing a class instance.
+
+        Args:
+            size (int): The size of the new square.
+        '''
+
+
         self.__size = size
