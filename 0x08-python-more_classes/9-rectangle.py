a2b8db8e570a8babd1df4e0ad60bafe04de9ce18
Grace Effiong <graceffiong@gmail.com>
2022-06-28 11:38:35 +0000

    task 1, 9 and 10

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -21,8 +21,8 @@ class Rectangle:
             height (int): The height of the new rectangle.
         """
         type(self).number_of_instances += 1
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
