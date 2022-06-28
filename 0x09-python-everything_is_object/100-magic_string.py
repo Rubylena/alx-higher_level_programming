38e814e3bde8dcb281287bee72afa1c0291b45df
Grace Effiong <graceffiong@gmail.com>
2022-06-28 20:03:43 +0000

    complete

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def magic_string(i=[]):
+    i += ["Holberton"]
+    return ", ".join(i)
+
+'''
+def magic_string():
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
+'''
