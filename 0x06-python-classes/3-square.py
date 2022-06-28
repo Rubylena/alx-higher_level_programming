d25c2dac8f32493b5eb06dbda4308b1434908a8f
Grace Effiong <graceffiong@gmail.com>
2022-06-28 11:00:12 +0000

    complete

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-''' defining a class - Square '''
+"""Define a class Square."""
 
 
 class Square:
-    ''' Represent a square '''
+    """Represent a square."""
 
-    def __init__(self, size):
-        ''' Intializing an instance of a class to raise error '''
+    def __init__(self, size=0):
+        """Initialize a new square.
 
+        Args:
+            size (int): The size of the new square.
+        """
         if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        elif (size < 0):
-            raise ValueError('size must be >= 0')
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
-        ''' method to check for area of a square '''
-
-        area = self.__size ** 2
-        return (area)
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,34 +1,31 @@
 #!/usr/bin/python3
-''' defining a class - Square '''
+"""Define a class Square."""
 
 
 class Square:
-    ''' Represent a square '''
+    """Represent a square."""
 
-    def __init__(self, size):
-        ''' Intializing an instance of a class to raise error '''
+    def __init__(self, size=0):
+        """Initialize a new square.
 
-        self.__size = size
+        Args:
+            size (int): The size of the new square.
+        """
+        self.size = size
 
     @property
     def size(self):
-        ''' getting the value of size '''
-
+        """Get/set the current size of the square."""
         return (self.__size)
 
     @size.setter
     def size(self, value):
-        ''' setting the value of size and making sure there are no errors '''
-
         if not isinstance(value, int):
-            raise TypeError('size must be an integer')
-        elif (value < 0):
-            raise ValueError('size must be >= 0')
-        else:
-            self.__size == value
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
-        ''' method to check for area of a square '''
-
-        area = self.__size ** 2
-        return (area)
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
