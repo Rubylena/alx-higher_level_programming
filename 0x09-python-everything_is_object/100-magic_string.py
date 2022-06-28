94ec5e50f8c96443422b17f1ab8d9f8b34276d91
Grace Effiong <graceffiong@gmail.com>
2022-06-28 20:34:45 +0000

    correction

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -2,9 +2,3 @@
 def magic_string(i=[]):
     i += ["BestSchool"]
     return ", ".join(i)
-
-'''
-def magic_string():
-    magic_string.n = getattr(magic_string, 'n', 0) + 1
-    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
-'''
