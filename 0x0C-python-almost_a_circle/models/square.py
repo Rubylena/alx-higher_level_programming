ca1b96518b36e005ecf16e72254528bca0ba9cde
Grace Effiong <graceffiong@gmail.com>
2022-07-12 22:07:22 +0000

    task 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -53,6 +53,3 @@ class Square(Rectangle):
                 self.x = kwargs["x"]
             if "y" in kwargs:
                 self.y = kwargs["y"]
-
-    def to_dictionary(self):
-        return ({"id": self.id, "size": self.width, "x": self.x, "y": self.y})
