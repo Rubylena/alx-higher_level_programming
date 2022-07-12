dfc4497ada906091ad7fede6b2075a33ea0a8a64
Grace Effiong <graceffiong@gmail.com>
2022-07-12 21:57:49 +0000

    task till 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -2,6 +2,7 @@
 """Defines a square class."""
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     """Represent a square."""
 
@@ -21,3 +22,38 @@ class Square(Rectangle):
         return ('[{}] ({:d}) {:d}/{:d} - {:d}'.format(Square.__name__,
                                                       self.id, self.x,
                                                       self.y, self.width))
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """updates multiple attributes"""
+        if len(args):
+            for i, a in enumerate(args):
+                # enumerate gives both the index and value respectively
+                if i == 0:
+                    self.id = a
+                elif i == 1:
+                    self.size = a
+                elif i == 2:
+                    self.x = a
+                elif i == 3:
+                    self.y = a
+        else:
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            if 'size' in kwargs:
+                self.width = kwargs['size']
+            if 'x' in kwargs:
+                self.x = kwargs['x']
+            if 'y' in kwargs:
+                self.y = kwargs['y']
+
+    def to_dictionary(self):
+        return ({'id': self.id, 'size': self.width, 'x': self.x, 'y': self.y})
