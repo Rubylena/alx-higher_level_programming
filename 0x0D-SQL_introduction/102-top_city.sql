702e961cd3a82191d32833e0cb071c7192f8bcf5
Grace Effiong <graceffiong@gmail.com>
2022-08-10 20:50:27 +0000

    adjust comments

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,4 +1,5 @@
--- display the top 3 cities with the highest temperature between july and august ordered by temp.
+-- display the top 3 cities with the highest temperature
+-- between july and august ordered by temp.
 SELECT CITY, AVG(value) AS average_temp
 FROM temperatures
 WHERE month = 7 OR month = 8
