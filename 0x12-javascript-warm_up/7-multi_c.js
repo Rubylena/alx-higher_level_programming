dc6edfd3f21bed095697dca6676cbdc1851ac774
Grace Effiong <graceffiong@gmail.com>
2022-09-06 09:58:38 +0000

    task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const x = process.argv[2];
-const change = parseInt(myVar);
+const change = parseInt(x);
 if (isNaN(change)) {
   console.log('Missing number of occurrences');
 } else {
-    for (let i = 0; i < x; i++)
-  console.log('C is fun');
+  for (let i = 0; i < x; i++) { console.log('C is fun'); }
 }
