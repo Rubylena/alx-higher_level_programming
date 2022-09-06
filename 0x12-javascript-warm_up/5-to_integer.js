6049ae156c0839c8e30ff760f1924d06fe5258a1
Ruby <graceffiong@gmail.com>
2022-09-06 10:38:25 +0100

    task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const myVar = process.argv[2];
+const change = parseInt(myVar);
+if (isNaN(change)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: $(change)`);
+}
