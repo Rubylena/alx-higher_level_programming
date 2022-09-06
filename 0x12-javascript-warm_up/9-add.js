1cd8c8fc0e721c6c50e8740b4cbbf55604ae115b
Grace Effiong <graceffiong@gmail.com>
2022-09-06 10:21:56 +0000

    task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,14 @@
 #!/usr/bin/node
-function add(a, b){
-  const a = process.argv[2];
-  const b = process.argv[3]
-  console.log(a + b);
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+function add (a, b) {
+  return (a + b);
 }
 
-add();
+console.log(add(a, b));
+/*
+const sum = (function add (a, b) {
+  return parseInt(a) + parseInt(b);
+})(process.argv[2], process.argv[3]);
+console.log(sum);
+*/
