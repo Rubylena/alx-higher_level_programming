a9b09416d604b8e3c4da7f1f83bf89517dae8419
Grace Effiong <graceffiong@gmail.com>
2022-09-12 21:49:12 +0000

    task 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""script that prints the first State object
+from the database hbtn_0e_6_usa"""
+from model_state import State, Base
+from sqlalchemy import (create_engine)
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+
+
+def model_state():
+    """initializate function model_state for db"""
+    state_engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1],
+        argv[2],
+        argv[3]),
+        pool_pre_ping=True)
+
+    # associate it with our custom Session class
+    State_Session = sessionmaker()
+    State_Session.configure(bind=state_engine)
+    session = State_Session()
+    rows = session.query(State).order_by(State.id).first()
+
+    if rows:
+        print('{}: {}'.format(rows.id, rows.name))
+    else:
+        print('Nothing')
+
+    session.close()
+
+
+if __name__ == '__main__':
+    model_state()
