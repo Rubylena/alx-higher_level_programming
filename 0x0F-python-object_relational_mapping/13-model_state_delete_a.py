675ce94f1565296d74456fa9c046673db7a3f6c1
Grace Effiong <graceffiong@gmail.com>
2022-09-12 23:15:08 +0000

    task 13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 """script that deletes all State objects with a name
-containing the letter a from the database hbtn_0e_6_usa"""
-from model_state import State, Base
-from sqlalchemy import create_engine
-from sys import argv
+containing the letter a from the database hbtn_0e_6_usa
+"""
+from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
+from sys import argv
+from sqlalchemy import create_engine
 
 
-def model_state_del():
+def model_state_delete():
     """function that delete the letter a"""
-    state_engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1],
         argv[2],
         argv[3]),
-        pool_pre_ping=True)
-
-    # associate it with our custom Session class
-    Base.metadata.create_all(state_engine)
-    State_Session = sessionmaker()
-    State_Session.configure(bind=state_engine)
-    session = State_Session()
+        pool_pre_ping=True
+    )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
 
     rows = session.query(State).order_by(State.id).all()
 
@@ -32,4 +32,4 @@ def model_state_del():
 
 
 if __name__ == '__main__':
-    model_state_del()
+    model_state_delete()
