ce896f85387c36d62830e27aba1a6c1c23b72166
Grace Effiong <graceffiong@gmail.com>
2022-09-12 23:29:07 +0000

    task 13

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -2,7 +2,7 @@
 """script that deletes all State objects with a name
 containing the letter a from the database hbtn_0e_6_usa
 """
-from model_state import Base, State
+from model_state import State
 from sqlalchemy.orm import sessionmaker
 from sys import argv
 from sqlalchemy import create_engine
