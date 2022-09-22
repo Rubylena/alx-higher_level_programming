825c56dc8b247a4da102ed11e0ab35ff81cd2f6c
GraceDev <graceffiong@gmail.com>
2022-09-22 08:34:06 -0700

    task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/bash
-# script that takes in a URL, sends a GET request to the URL,
-# and displays the body of the response
-response=$(curl -s -o /dev/null -w "%{http_code}" "$1")
-status=200
-if [ "$response" -eq "$status" ]
-then
-  curl -sb -H "$1"
-fi
+# sends a GET request to the URL, displays the body of the response
+curl -sL "$1"
