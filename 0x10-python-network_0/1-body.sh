a5dc3b3ce8516323fe07e3595561e1a8b530e701
GraceDev <graceffiong@gmail.com>
2022-09-22 08:17:30 -0700

    200 status code

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/bash
 # script that takes in a URL, sends a GET request to the URL,
 # and displays the body of the response
-curl -s "$1"
+response=$(curl -s -o /dev/null -w "%{http_code}" "$1")
+status=200
+if [ "$response" -eq "$status" ]
+then
+  curl -sb -H "$1"
+fi
