531eb75aab066030c54068a9fd0a5a5923c73389
Grace 2 <graceffiong@gmail.com>
2022-09-26 08:32:48 -0700

    task 10

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -5,15 +5,18 @@ from sys import argv
 
 
 def get_commits():
-    """display Github id"""
+    """display commits and author"""
     url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
     req = requests.get(url)
     json = req.json()
 
-    for i in range(0, 10):
-        sha = json[i].get('sha')
-        author = json[i].get('commit').get('author').get('name')
-        print('{}: {}'.format(sha, author))
+    try:
+        for i in range(0, 10):
+            sha = json[i].get('sha')
+            author = json[i].get('commit').get('author').get('name')
+            print('{}: {}'.format(sha, author))
+    except IndexError:
+        pass
 
 
 if __name__ == '__main__':
