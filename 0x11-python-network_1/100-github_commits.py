c57206183480bb7c3166c4b4cb58f6081a2148de
Grace 2 <graceffiong@gmail.com>
2022-09-26 08:18:04 -0700

    first 10

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""initialize"""
+import requests
+from sys import argv
+
+
+def get_commits():
+    """display Github id"""
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[1], argv[2])
+    print(url)
+    req = requests.get(url)
+    json = req.json()
+    try:
+        for i in range(0, 10):
+            sha = json[i].get('sha')
+            author = json[i].get('commit').get('author').get('name')
+            print('{}: {}'.format(sha, author))
+    except IndexError:
+        pass
+
+
+if __name__ == '__main__':
+    get_commits()
