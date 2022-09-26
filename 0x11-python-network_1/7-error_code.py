fb61ef1b7114765fcf46b5dd7de8910984673048
Grace 2 <graceffiong@gmail.com>
2022-09-26 05:55:43 -0700

    task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,13 +5,14 @@ from sys import argv
 
 
 def request_status():
+    """request status code"""
     req = requests.get(argv[1])
     code = req.status_code
 
     if(code == 200):
         print(req.text)
     else:
-        print('Error code: ', req.status_code)
+        print('Error code: {}'.format(req.status_code))
 
 
 if __name__ == '__main__':
