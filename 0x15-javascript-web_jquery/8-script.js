13e09da276628ef831645250025b1d12a8b720b1
Ruby <graceffiong@gmail.com>
2022-10-27 00:46:19 +0100

    web scraping

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,3 +1,8 @@
-$.get('https://swapi-api.hbtn.io/api/films/?format=json', function(data){
-    $('ul#list_movies').append(...data.results.map(data => `<li>${data.title}<li>`))
+// $.get('https://swapi-api.hbtn.io/api/films/?format=json', function(data){
+//     $('ul#list_movies').append(...data.results.map(data => `<li>${data.title}<li>`))
+// });
+$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+  $.each(data.results, function (index, value) {
+    $('<li>' + value.title + '</li>').appendTo('UL#list_movies');
+  });
 });
