528c060f10809261487ecaec85069f0c36ae42b7
Grace <graceffiong@gmail>
2022-10-27 00:09:29 +0000

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', (error, data) => {
-	if(error) {
-		console.log(error || data);
-	}
+fs.readFile(process.argv[2], 'utf8', function (error, data) {
+  if (error) {
+    console.log(error || data);
+  }
 });
