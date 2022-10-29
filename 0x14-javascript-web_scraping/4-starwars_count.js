1db779837319fb89de1e5cbfa0c444e773063858
Ruby <graceffiong@gmail.com>
2022-10-29 23:05:45 +0100

    correct output

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,10 +3,15 @@ const request = require('request');
 request.get(process.argv[2], function (error, response, body) {
   if (error) { console.log(error); }
   const result = JSON.parse(body).results;
-  let counter = 0;
-  result.map(data => {
-    const wedgeAntilles = 'https://swapi-api.hbtn.io/api/people/18/';
-    return (data.characters.includes(wedgeAntilles)) ? counter++ : counter;
-  }, 0);
+  // let counter = 0;
+  // result.map(data => {
+  //   const wedgeAntilles = 'https://swapi-api.hbtn.io/api/people/18/';
+  //   return (data.characters.includes(wedgeAntilles)) ? counter++ : counter;
+  // }, 0);
   // console.log(counter);
+  console.log(result.reduce((count, movie) => {
+    return movie.characters.find((character) => character.endsWith('/18/'))
+      ? count + 1
+      : count;
+  }, 0));
 });
